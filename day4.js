0e4137af311a746238029a4f0c133e379c9e5e0d
Benjamin Boll <boll@sipgate.de>
2022-03-18 14:09:59 +0100

    :sparkles: solve day 4

---
+++ day4.js
@@ -0,0 +1,30 @@
+const inputData = require('./day4-input.json');
+const md5 = require('js-md5');
+
+
+function one() {
+    const input = inputData.one;
+    let md5Try;
+    let output = "1";
+    let hash = 1;
+    while (!output.startsWith("00000")) {
+        md5Try = input + (hash++)
+        output = md5(md5Try);
+    }
+    console.log(md5Try);
+}
+
+function two() {
+    const input = inputData.two;
+    let md5Try;
+    let output = "1";
+    let hash = 1;
+    while (!output.startsWith("000000")) {
+        md5Try = input + (hash++)
+        output = md5(md5Try);
+    }
+    console.log(md5Try);
+}
+
+//one();
+two();
