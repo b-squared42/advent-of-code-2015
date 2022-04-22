736ecf570e9c569bee25f9c561d1325005d0fae9
Benjamin Boll <boll@sipgate.de>
2022-04-22 08:43:24 +0200

    :sparkles: solve day 6

--- day6.js
+++ day6.js
@@ -4,56 +4,86 @@ function one() {
     const input = inputData.one;
     const sentences = input.split("|");
 
-    let field = Array.from(Array(1000), () => Array(1000).fill(false));
-    sentences.forEach(sentence => {
-        if (sentence.startsWith("turn on")) {
-            sentence = sentence.slice("turn on ".length);
-            const mesurements = sentence.split(" through ");
-            const from = mesurements[0].split(",");
-            const to = mesurements[1].split(",");
-            for (let x = from[0]; x <= to[0]; x++) {
-                for (let y = from[1]; y <= to[1]; y++) {
-                    field[x][y] = true;
-                }
-            }
-        } else if (sentence.startsWith("turn off")) {
-            sentence = sentence.slice("turn off ".length);
-            const mesurements = sentence.split(" through ");
-            const from = mesurements[0].split(",");
-            const to = mesurements[1].split(",");
-            for (let x = from[0]; x <= to[0]; x++) {
-                for (let y = from[1]; y <= to[1]; y++) {
-                    field[x][y] = false;
-                }
-            }
-        } else if (sentence.startsWith("toggle")) {
-            sentence = sentence.slice("toggle ".length);
-            const mesurements = sentence.split(" through ");
-            const from = mesurements[0].split(",");
-            const to = mesurements[1].split(",");
-            for (let x = from[0]; x <= to[0]; x++) {
-                for (let y = from[1]; y <= to[1]; y++) {
-                    field[x][y] = !field[x][y];
-                }
+    let field = [];
+    for (let i = 0; i < 1000; i++) {
+        let arr = [];
+        for (let j = 0; j < 1000; j++) {
+            arr.push(false);
+        }
+        field.push(arr);
+    }
+
+    for (let i = 0; i < sentences.length; i++) {
+        const parts = sentences[i].split(/,| /);
+        if (parts[0] === "turn") {
+            for (let x = parseInt(parts[2]); x <= parseInt(parts[5]); x++) {
+                for (let y = parseInt(parts[3]); y <= parseInt(parts[6]); y++) {
+                    field[x][y] = (parts[1] === "on");
+                }   
             }
         } else {
-            console.log("ERROR");
+            for (let x = parseInt(parts[1]); x <= parseInt(parts[4]); x++) {
+                for (let y = parseInt(parts[2]); y <= parseInt(parts[5]); y++) {
+                    field[x][y] = !field[x][y];
+                }   
+            }
         }
-    })
+    }
     let litLights = 0;
     for (let x = 0; x < field.length; x++) {
         for (let y = 0; y < field[0].length; y++) {
             if (field[x][y]) {
                 litLights++;
-            }
+            }   
         }
     }
     console.log(litLights);
 }
 
 function two() {
-    
+    const input = inputData.one;
+    const sentences = input.split("|");
+
+    let field = [];
+    for (let i = 0; i < 1000; i++) {
+        let arr = [];
+        for (let j = 0; j < 1000; j++) {
+            arr.push(0);
+        }
+        field.push(arr);
+    }
+
+    for (let i = 0; i < sentences.length; i++) {
+        const parts = sentences[i].split(/,| /);
+        if (parts[0] === "turn") {
+            for (let x = parseInt(parts[2]); x <= parseInt(parts[5]); x++) {
+                for (let y = parseInt(parts[3]); y <= parseInt(parts[6]); y++) {
+                    if (parts[1] === "on") {
+                        field[x][y] += 1;
+                    } else {
+                        field[x][y] -= 1;
+                        if (field[x][y] < 0) {
+                            field[x][y] = 0;
+                        }
+                    }
+                }   
+            }
+        } else {
+            for (let x = parseInt(parts[1]); x <= parseInt(parts[4]); x++) {
+                for (let y = parseInt(parts[2]); y <= parseInt(parts[5]); y++) {
+                    field[x][y] += 2
+                }   
+            }
+        }
+    }
+    let brightness = 0;
+    for (let x = 0; x < field.length; x++) {
+        for (let y = 0; y < field[0].length; y++) {
+            brightness += field[x][y];  
+        }
+    }
+    console.log(brightness);
 }
 
-one();
+//one();
 two();
